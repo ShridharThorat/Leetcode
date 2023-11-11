2e178657ee03cbfea7d03566fa05669adeef25ca
Shridhar <shridhar.thorat01+github@gmail.com>
2023-11-11 23:59:13 +1000

    Ignoring bin and obj folders

---
+++ Y - Name/Solution2/Program2.cs
@@ -0,0 +1,12 @@
+﻿namespace Solution;
+public class Solution2 {
+
+    public static string ex2()
+    {
+        return "Example 2";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(ex2());
+    }
+}
