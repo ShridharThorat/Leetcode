88ca690cdf3198e640cc91d793dc41b8fbcaf083
Shridhar <shridhar.thorat01+github@gmail.com>
2023-11-12 00:16:07 +1000

    Put back original files

---
+++ X - Name/Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Solution;
+public class Solution1 {
+
+    public static String ex1()
+    {
+        return "Example 1";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(ex1());
+    }
+}
