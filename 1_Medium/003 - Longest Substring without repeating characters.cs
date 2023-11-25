786e57498169c0094e7e67e42c3be1f109efc704
Shridhar <shridhar.thorat01+github@gmail.com>
2023-11-25 13:48:42 +1000

    Rename folders

--- 1_Medium/003 - Longest Substring without repeating characters.cs
+++ 1_Medium/003 - Longest Substring without repeating characters.cs
@@ -26,4 +26,21 @@ public class _003_
         max = Math.Max(max, currentSubString.Count);
         return max;
     }
+    // public static void Main()
+    // {
+    //     // Sample Linked Lists
+    //     ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    //     ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+    //     ListNode l3 = new ListNode(9, new ListNode(9, new ListNode(9)));
+    //     ListNode l4 = new ListNode(1);
+    //     ListNode? result1 = _002_.AddTwoNumbers(l1, l4);
+
+    //     while (result1 != null)
+    //     {
+    //         Console.Write($"{result1.val} ");
+    //         result1 = result1.next;
+    //     }
+
+    // }
 }
---
+++ 1_Medium/151 - Reverse words in a string.cs
@@ -0,0 +1,24 @@
+﻿namespace Medium;
+
+public class _151_
+{
+    public static string ReverseWords(string s)
+    {
+        // O(n) runtime and memory -> n is number of words
+        string[] list_s = s.Split(" ");
+
+
+        return s;
+    }
+
+    public static void Main()
+    {
+        string s = "the sky is blue";
+        string output = _151_.ReverseWords(s);
+
+
+        s = "  hello world  ";
+
+        s = "a good   example";
+    }
+}
